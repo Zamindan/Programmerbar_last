232d2304b7bb1fbcfc9fbba11c68f28ee4bc89f4
Sondre Pettersen <sondre.pettersen@hotmail.com>
2025-02-03 15:23:31 +0100

    I2C test

--- main/main.c
+++ main/main.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
 #include "driver/gpio.h"
-#include "driver/sdm.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_adc/adc_oneshot.h"
-#include "driver/ledc.h"
+#include "driver/i2c_master.h"
+#include "driver/i2c_slave.h"
 
+#define PORT_NUMBER -1
+#define SCL_IO_PIN GPIO_NUM_12
+#define SDA_IO_PIN GPIO_NUM_13
+#define data 0xFF0000
 void app_main(void)
 {
-    
+    i2c_master_bus_config_t i2c_mst_config = {
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .i2c_port = PORT_NUMBER,
+        .scl_io_num = SCL_IO_PIN,
+        .sda_io_num = SDA_IO_PIN,
+        .glitch_ignore_cnt = 7,
+        .flags.enable_internal_pullup = true,
+    };
+    i2c_master_bus_handle_t bus_handle;
+
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
+
+
+    i2c_device_config_t dev_cfg = {
+        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+        .device_address = 0x2A,
+        .scl_speed_hz = 100000,
+    };
+    i2c_master_dev_handle_t dev_handle;
+
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
+
+    ESP_ERROR_CHECK(i2c_master_transmit(dev_handle, &data, 100, -1));
 }
