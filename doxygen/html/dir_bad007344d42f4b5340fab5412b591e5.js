2dc9cbd83d5bf2b4dd49399ed58c19133bc07ae3
Sondre Pettersen <sondre.pettersen@hotmail.com>
2025-02-05 14:34:08 +0100

    Lagt til Doxygen

---
+++ doxygen/html/dir_bad007344d42f4b5340fab5412b591e5.js
@@ -0,0 +1,4 @@
+var dir_bad007344d42f4b5340fab5412b591e5 =
+[
+    [ "adc.h", "adc_8h_source.html", null ]
+];
---
+++ doxygen/html/navtreeindex0.js
@@ -0,0 +1,9 @@
+var NAVTREEINDEX0 =
+{
+"adc_8h_source.html":[0,0,0,0,0],
+"dir_5c982d53a68cdbcd421152b4020263a9.html":[0,0,0],
+"dir_bad007344d42f4b5340fab5412b591e5.html":[0,0,0,0],
+"files.html":[0,0],
+"index.html":[],
+"pages.html":[]
+};
